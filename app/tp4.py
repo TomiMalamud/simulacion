6ac1527626ad05e14d201a427c48a0d31135062c
Tomi Malamud <tomasmalamud@gmail.com>
2024-06-22 10:28:57 -0300

    optimizacion codigo

--- app/tp4.py
+++ app/tp4.py
@@ -1,107 +1,10 @@
 import random
-import math
 from flask import Flask, Blueprint, render_template, request
+from .tp4utils import *
+import time
 
 app = Flask(__name__)
 tp4 = Blueprint("tp4", __name__, template_folder="templates")
-
-
-def exponential_random(mean, rnd):
-    while rnd == 1.0:
-        rnd = random.random()
-    return -mean * math.log(1 - rnd)
-
-
-def initialize_means(
-    checkin_arrivals,
-    ends_checkin,
-    security_arrivals,
-    ends_security,
-    passport_arrivals,
-    ends_passport,
-    boarding_arrivals,
-    ends_boarding,
-):
-    return {
-        "checkin": 60 / checkin_arrivals,
-        "security": 60 / security_arrivals,
-        "passport": 60 / passport_arrivals,
-        "boarding": 60 / boarding_arrivals,
-        "checkin_service": 60 / ends_checkin,
-        "security_service": 60 / ends_security,
-        "passport_service": 60 / ends_passport,
-        "boarding_service": 60 / ends_boarding,
-    }
-
-
-def create_initial_row(means):
-    initial_row = {"row_id": 0, "event": "Initialization", "clock": 0}
-
-    for process in means:
-        if "_service" not in process:
-            initial_row[f"{process}_arrival_rnd"] = f"{random.random():.4f}"
-            initial_row[f"{process}_arrival_time_between"] = ""
-            initial_row[f"{process}_arrival_next"] = ""
-            initial_row[f"end_{process}_rnd"] = ""
-            initial_row[f"end_{process}_time"] = ""
-            for server_id in range(1, 4 if process in ["checkin", "boarding"] else 3):
-                initial_row[f"end_{process}_{server_id}"] = float("inf")
-                initial_row[f"{process}_state_{server_id}"] = "Free"
-            initial_row[f"{process}_queue"] = 0
-            initial_row[f"ac_waiting_time_{process}"] = 0
-            initial_row[f"passengers_waited_{process}"] = 0
-
-    return initial_row
-
-
-def update_service_state(
-    new_row, event_name, means, clock, passenger_states, event_id_map, passenger_id
-):
-    server_count = 3 if event_name in ["checkin", "boarding"] else 2
-    for server_id in range(1, server_count + 1):
-        if new_row[f"{event_name}_state_{server_id}"] == "Free":
-            new_row[f"{event_name}_state_{server_id}"] = "Busy"
-            rnd_end = random.random()
-            end_time = exponential_random(means[f"{event_name}_service"], rnd_end)
-            new_row[f"end_{event_name}_rnd"] = f"{rnd_end:.4f}"
-            new_row[f"end_{event_name}_time"] = f"{end_time:.2f}"
-            end_clock = clock + end_time
-            new_row[f"end_{event_name}_{server_id}"] = f"{end_clock:.2f}"
-            event_id = event_id_map[event_name]
-            event_id_map[f"end_{event_name}_{server_id}"] = (
-                f"{event_name.capitalize()[:3]}_{event_id}",
-                passenger_id,
-                end_clock,
-            )
-            passenger_states[passenger_id] = (
-                f"in_{event_name} {event_name.capitalize()[:3]}_{event_id}"
-            )
-            new_row[f"passenger_{passenger_id}_state"] = passenger_states[passenger_id]
-            return True
-    return False
-
-
-def handle_queue(
-    new_row, event_name, means, clock, passenger_states, event_id_map, passenger_id_map
-):
-    if new_row[f"{event_name}_queue"] > 0:
-        new_row[f"{event_name}_queue"] -= 1
-        queued_passengers = [
-            id
-            for id, state in passenger_states.items()
-            if state == f"in_queue_{event_name}"
-        ]
-        if queued_passengers:
-            passenger_id = min(queued_passengers)
-            update_service_state(
-                new_row,
-                event_name,
-                means,
-                clock,
-                passenger_states,
-                event_id_map,
-                passenger_id,
-            )
 
 
 def simulate(
@@ -133,7 +36,7 @@ def simulate(
     event_id_map = {process: 0 for process in means if "_service" not in process}
     passenger_id_map = {"current_passenger_id": 0}
     passenger_states = {}
-    active_passengers = set()  # New set to track active passengers
+    passengers_in_range = set()  # New set to track active passengers
 
     # Initialize arrival times
     for process in means:
@@ -246,11 +149,6 @@ def simulate(
                 new_row[f"passenger_{new_passenger_id}_state"] = passenger_states[
                     new_passenger_id
                 ]
-        for key, state in passenger_states.items():
-            if state != "-":
-                active_passengers.add(key)
-            elif key in active_passengers:
-                active_passengers.remove(key)
 
         # Ensure passenger states are updated in the new row
         for key in passenger_states:
@@ -261,8 +159,19 @@ def simulate(
         if row_count >= start_row and rows_shown < additional_rows:
             rows_to_show.append(new_row)
             rows_shown += 1
+            passengers_in_range.update(
+                key for key in passenger_states if f"passenger_{key}_state" in new_row
+            )
 
         row_count += 1
+    passengers_in_range = set(sorted(passengers_in_range)[:100])
+    # Remove passenger state information for passengers not in passengers_in_range
+    for row in rows_to_show:
+        for key in list(row.keys()):
+            if key.startswith("passenger_") and key.endswith("_state"):
+                passenger_id = int(key.split("_")[1])
+                if passenger_id not in passengers_in_range:
+                    del row[key]
 
     if rows_to_show[-1] != all_rows[-1]:
         rows_to_show.append(all_rows[-1])
@@ -274,7 +183,7 @@ def simulate(
 
     passenger_count = passenger_id_map["current_passenger_id"]
 
-    return rows_to_show, passenger_count, active_passengers
+    return rows_to_show, passenger_count, passengers_in_range
 
 
 @tp4.route("/tp4", methods=["GET"])
@@ -283,7 +192,7 @@ def tp4_render():
     additional_rows = int(request.args.get("additional_rows", default=100, type=int))
     total_rows = int(request.args.get("total_rows", default=100, type=int))
     checkin_arrivals = int(request.args.get("checkin_arrivals", default=50, type=int))
-    checkin_servers = int(request.args.get('checkin_servers', default=3, type=int))
+    checkin_servers = int(request.args.get("checkin_servers", default=3, type=int))
     ends_checkin = int(request.args.get("ends_checkin", default=15, type=int))
     security_arrivals = int(request.args.get("security_arrivals", default=40, type=int))
     ends_security = int(request.args.get("ends_security", default=20, type=int))
@@ -291,12 +200,13 @@ def tp4_render():
     ends_passport = int(request.args.get("ends_passport", default=12, type=int))
     boarding_arrivals = int(request.args.get("boarding_arrivals", default=60, type=int))
     ends_boarding = int(request.args.get("ends_boarding", default=25, type=int))
+    start_time = time.time()  # Start the timer
 
-    data, passenger_count, active_passengers = simulate(
+    data, passenger_count, passengers_in_range = simulate(
         start_row,
         additional_rows,
         total_rows,
-        checkin_arrivals,    
+        checkin_arrivals,
         ends_checkin,
         security_arrivals,
         ends_security,
@@ -305,13 +215,19 @@ def tp4_render():
         boarding_arrivals,
         ends_boarding,
     )
+
+    end_time = time.time()  # End the timer
+    processing_time = end_time - start_time  # Calculate the elapsed time
+
+    print(f"Processing time: {processing_time:.2f} seconds")  # Print the processing time
+
     return render_template(
         "tp4.html",
         data=data,
         passenger_count=passenger_count,
-        active_passengers=active_passengers,
+        passengers_in_range=passengers_in_range,
         request=request,
-        checkin_servers=checkin_servers,        
+        checkin_servers=checkin_servers,
     )
 
 
---
+++ app/tp4utils.py
@@ -0,0 +1,101 @@
+import math
+import random
+
+
+def exponential_random(mean, rnd):
+    while rnd == 1.0:
+        rnd = random.random()
+    return -mean * math.log(1 - rnd)
+
+
+def initialize_means(
+    checkin_arrivals,
+    ends_checkin,
+    security_arrivals,
+    ends_security,
+    passport_arrivals,
+    ends_passport,
+    boarding_arrivals,
+    ends_boarding,
+):
+    return {
+        "checkin": 60 / checkin_arrivals,
+        "security": 60 / security_arrivals,
+        "passport": 60 / passport_arrivals,
+        "boarding": 60 / boarding_arrivals,
+        "checkin_service": 60 / ends_checkin,
+        "security_service": 60 / ends_security,
+        "passport_service": 60 / ends_passport,
+        "boarding_service": 60 / ends_boarding,
+    }
+
+
+def create_initial_row(means):
+    initial_row = {"row_id": 0, "event": "Initialization", "clock": 0}
+
+    for process in means:
+        if "_service" not in process:
+            initial_row[f"{process}_arrival_rnd"] = f"{random.random():.4f}"
+            initial_row[f"{process}_arrival_time_between"] = ""
+            initial_row[f"{process}_arrival_next"] = ""
+            initial_row[f"end_{process}_rnd"] = ""
+            initial_row[f"end_{process}_time"] = ""
+            for server_id in range(1, 4 if process in ["checkin", "boarding"] else 3):
+                initial_row[f"end_{process}_{server_id}"] = float("inf")
+                initial_row[f"{process}_state_{server_id}"] = "Free"
+            initial_row[f"{process}_queue"] = 0
+            initial_row[f"ac_waiting_time_{process}"] = 0
+            initial_row[f"passengers_waited_{process}"] = 0
+
+    return initial_row
+
+
+def update_service_state(
+    new_row, event_name, means, clock, passenger_states, event_id_map, passenger_id
+):
+    server_count = 3 if event_name in ["checkin", "boarding"] else 2
+    for server_id in range(1, server_count + 1):
+        if new_row[f"{event_name}_state_{server_id}"] == "Free":
+            new_row[f"{event_name}_state_{server_id}"] = "Busy"
+            rnd_end = random.random()
+            end_time = exponential_random(means[f"{event_name}_service"], rnd_end)
+            new_row[f"end_{event_name}_rnd"] = f"{rnd_end:.4f}"
+            new_row[f"end_{event_name}_time"] = f"{end_time:.2f}"
+            end_clock = clock + end_time
+            new_row[f"end_{event_name}_{server_id}"] = f"{end_clock:.2f}"
+            event_id = event_id_map[event_name]
+            event_id_map[f"end_{event_name}_{server_id}"] = (
+                f"{event_name.capitalize()[:3]}_{event_id}",
+                passenger_id,
+                end_clock,
+            )
+            passenger_states[passenger_id] = (
+                f"in_{event_name} {event_name.capitalize()[:3]}_{event_id}"
+            )
+            new_row[f"passenger_{passenger_id}_state"] = passenger_states[passenger_id]
+            return True
+    return False
+
+
+def handle_queue(
+    new_row, event_name, means, clock, passenger_states, event_id_map, passenger_id_map
+):
+    if new_row[f"{event_name}_queue"] > 0:
+        new_row[f"{event_name}_queue"] -= 1
+        queued_passengers = [
+            id
+            for id, state in passenger_states.items()
+            if state == f"in_queue_{event_name}"
+        ]
+        if queued_passengers:
+            passenger_id = min(queued_passengers)
+            update_service_state(
+                new_row,
+                event_name,
+                means,
+                clock,
+                passenger_states,
+                event_id_map,
+                passenger_id,
+            )
+
